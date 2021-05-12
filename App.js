f4f77b1153db7181e7b51eb6b79ecdeec9b8ac89
harksu <harksulim@naver.com>
2021-05-12 14:45:45 +0900

    와 레전드네 이거 안누르고 여태 안된다고 한거였어?

--- App.js
+++ App.js
@@ -13,6 +13,10 @@ import PaybackMain from './front/pointMarket/payback/PaybackMain';
 import Payback from './front/pointMarket/payback/Payback';
 import Payment from './front/pointMarket/Payment';
 import PayResult from './front/pointMarket/PayResult';
+import Makequest from './front/main/MakeQuest';
+import Meal from './front/main/Meal';
+import MakequestDetail from './front/main/MakeQuestDetail';
+
 
 
 
@@ -49,6 +53,11 @@ export class App extends Component {
                     <Stack.Screen name="Payback" component={Payback} navigation={this.props.navigation} />
                     <Stack.Screen name="Payment" component={Payment} navigation={this.props.navigation} />
                     <Stack.Screen name="PayResult" component={PayResult} navigation={this.props.navigation} />
+                    <Stack.Screen name="Makequest" component={Makequest}navigation={this.props.navigation} />
+                    <Stack.Screen name="Meal" component={Meal}navigation={this.props.navigation} />
+                    <Stack.Screen name="MakequestDetail" component={MakequestDetail}navigation={this.props.navigation} />
+                   
+
 
 
                     {/* Tabs */}
